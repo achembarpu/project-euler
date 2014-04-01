ec9a00686f9af7cb1c89f93abe241d9953581140
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-01 22:32:40 +0530

    clarified given data
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

--- src/solutions/001/001_arvindch.py
+++ src/solutions/001/001_arvindch.py
@@ -4,9 +4,10 @@ __email__ = 'achembarpu@gmail.com'
 
 def main():
     
+    giv_lim = 1000
     req_sum = 0
     
-    for x in xrange(1000):
+    for x in xrange(giv_lim):
         if x % 3 == 0 or x % 5 == 0:
             req_sum += x
     
