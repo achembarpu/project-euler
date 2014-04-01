0475d593a8fc8705bd50fbf1bfb73baffc38f5be
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-01 22:33:08 +0530

    solved problem two
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

---
+++ src/solutions/002/002_arvindch.py
@@ -0,0 +1,24 @@
+__author__ = 'Arvind Chembarpu'
+__email__ = 'achembarpu@gmail.com'
+
+
+from src.custom import gens
+
+
+def main():
+    
+    giv_lim = 4000000
+    req_sum = 0
+    
+    for fib_num in gens.fibonacci_gen():
+        if fib_num > giv_lim:
+            break
+        if fib_num % 2 == 0:
+            req_sum += fib_num
+    
+    return req_sum
+
+
+if __name__ == '__main__':
+    answer = main()
+    print answer
