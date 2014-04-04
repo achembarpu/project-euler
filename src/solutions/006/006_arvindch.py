79b11df0aa9227a58cd9fd546f468e4e4ad7a5ab
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-04 09:33:52 +0530

    clarified given data
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

--- src/solutions/006/006_arvindch.py
+++ src/solutions/006/006_arvindch.py
@@ -4,12 +4,12 @@ __email__ = 'achembarpu@gmail.com'
 
 def main():
     
-    giv_num = 100
+    giv_n = 100
     req_diff = 0
     
-    sum_of_squares = (giv_num * (giv_num + 1) * ((2 * giv_num) + 1)) / 6
-    square_of_sums = ((giv_num**2) * ((giv_num + 1)**2)) / 4
-    req_diff = square_of_sums - sum_of_squares
+    sum_of_squares = (giv_n * (giv_n + 1) * ((2 * giv_n) + 1)) / 6
+    square_of_sum = ((giv_n**2) * ((giv_n + 1)**2)) / 4
+    req_diff = square_of_sum - sum_of_squares
 
     return req_diff
 
