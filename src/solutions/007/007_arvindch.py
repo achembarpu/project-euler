068d97f5e291f3d88f41dd5ffc7224138d146a72
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-05 15:18:37 +0530

    solved problem seven
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

---
+++ src/solutions/007/007_arvindch.py
@@ -0,0 +1,20 @@
+__author__ = 'Arvind Chembarpu'
+__email__ = 'achembarpu@gmail.com'
+
+
+from src.custom import misc
+
+
+def main():
+
+    giv_n = 10001
+    req_num = 0
+    
+    req_num = misc.nth_prime(giv_n)
+
+    return req_num
+
+
+if __name__ == '__main__':
+    answer = main()
+    print answer
