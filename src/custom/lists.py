02ef69ec335658bfb684ce894eb714a3e9fdde39
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-14 20:25:32 +0530

    improved primes_list performance by 50%
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

--- src/custom/lists.py
+++ src/custom/lists.py
@@ -1,14 +1,22 @@
 # Lists
 
 
-import itertools
-
-from src.custom import gens, params
+from src.custom import params
 
 
 def primes_list(limit=params.prime_lim):
     """
     Returns a list of prime numbers
+    Uses the Sieve of Sundaram method
     """
-    primes = list(itertools.takewhile(lambda p: p < limit, gens.primes_gen()))
-    return primes
+    numbers = range(3, limit, 2)
+    half = int(limit / 2)
+    initial = 4
+
+    for step in xrange(3, limit, 2):
+        for i in xrange(initial, half, step):
+            numbers[i-1] = None
+        initial += 2 * (step + 1)
+
+        if initial > half:
+            return [2] + filter(None, numbers)
