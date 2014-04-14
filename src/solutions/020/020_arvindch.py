ee195013e1c2d1748ac722ce69ff99abbd7407fc
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-14 11:07:01 +0530

    solved problem twenty
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

---
+++ src/solutions/020/020_arvindch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+
+__author__ = 'Arvind Chembarpu'
+__email__ = 'achembarpu@gmail.com'
+
+
+import math
+
+
+def main():
+    
+    giv_num = math.factorial(100)
+    req_sum = 0
+    
+    for digit in str(giv_num):
+        req_sum += int(digit)
+    
+    return req_sum
+
+
+if __name__ == '__main__':
+    answer = main()
+    print answer
