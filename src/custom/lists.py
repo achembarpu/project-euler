0e915a50108d8fcb61adbc69e2c5ab1be8827d57
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-15 16:25:24 +0530

    fixed minor regression and tweaked algorithm
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

--- src/custom/lists.py
+++ src/custom/lists.py
@@ -9,18 +9,14 @@ def primes_list(limit=params.prime_lim):
     Returns a list of prime numbers
     Using a tweaked Sieve of Eratosthenes method
     """
-    half = limit//2
+    half = limit//2 + 1
     sieve = [False, True] * half
     sieve[1], sieve[2] = False, True
     i = 3
     while i < half:
         for j in xrange(i*i, limit, i):
             sieve[j] = False
-        i += 1
-        try:
-            while not sieve[i]:
-                i += 1
-        except IndexError:
-            break
-    else:
-        return [i for i in xrange(limit) if sieve[i]]
+        i += 2
+        while not sieve[i]:
+            i += 1
+    return [i for i in xrange(limit) if sieve[i]]
