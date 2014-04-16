7d8f3388b6da03ccab9892239904ce0864725fe5
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-16 23:08:23 +0530

    solved problem 040
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

---
+++ src/solutions/040/040_arvindch.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+
+__author__ = 'Arvind Chembarpu'
+__email__ = 'achembarpu@gmail.com'
+
+
+def main():
+    
+    def digit_gen():
+        """
+        Generates digits of the natural numbers sequence
+        """
+        number = 0
+        while True:
+            number += 1
+            for digit in str(number):
+                yield int(digit)
+    
+    giv_last_dig = 10 ** 6
+    digits = digit_gen()
+    decimal_constant = [next(digits) for _ in xrange(giv_last_dig)]
+    req_prod = 1
+    
+    i = 1
+    while i <= giv_last_dig:
+        req_prod *= decimal_constant[i - 1]
+        i *= 10
+    
+    return req_prod
+
+
+if __name__ == '__main__':
+    answer = main()
+    print answer
