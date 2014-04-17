ffd88953b447a88ed176c6c21c28a667b49d4497
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-17 10:13:31 +0530

    minor tweaks
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

--- src/solutions/009/009_arvindch.py
+++ src/solutions/009/009_arvindch.py
@@ -17,27 +17,25 @@ def main():
         """
         try:
             for a in xrange(start, end):
-                for b in xrange(start, end):
+                for b in xrange(a, end):
                     c = math.sqrt(a ** 2 + b ** 2)
                     if checks.is_int(c):
-                        triplet = [a, b, int(c)]
-                        yield triplet
+                        t = [a, b, int(c)]
+                        yield t
         except IndexError:
             raise StopIteration
     
     giv_sum = 1000
     inf_lim = giv_sum / 2
     req_prod = 0
-
-    try:
-        for triplet in pythagorean_triplet_gen(2, inf_lim):
-            if misc.list_sum(triplet) == giv_sum:
-                req_prod = int(misc.list_product(triplet))
-                raise excepts.BreakNestedLoop
-    except excepts.BreakNestedLoop:
-        pass
-    finally:
-        return req_prod
+    
+    pythagorean_triplets = pythagorean_triplet_gen(2, inf_lim)
+    
+    for t in pythagorean_triplets:
+        if misc.list_sum(t) == giv_sum:
+            req_prod = int(misc.list_product(t))
+            break
+    return req_prod
 
 
 if __name__ == '__main__':
