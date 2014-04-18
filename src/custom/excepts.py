16630124bc1ca18ead9e2098ad699146a841ad9f
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-18 12:50:31 +0530

    implemented function timeout handler
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

--- src/custom/excepts.py
+++ src/custom/excepts.py
@@ -6,3 +6,10 @@ class BreakNestedLoop(Exception):
     Raise in a try-except to easily exit a Nested Loop
     """
     pass
+
+
+class TimeoutError(Exception):
+    """
+    Raise in a try-except with Timeout()
+    """
+    pass
--- src/custom/tools.py
+++ src/custom/tools.py
@@ -1,15 +1,17 @@
 # Tools
 
 
+import signal
 import time
 
+from src.custom import excepts
 
 class Timer(object):
     """
-    A Timer implementation, to easily time code-snippet execution
+    A Timer tool, to time code-snippet execution
     Use as:
     with Timer():
-        #code
+        # code
     """
     def __init__(self):
         self.start = 0.0
@@ -24,3 +26,27 @@ class Timer(object):
         self.interval = self.end - self.start
         
         print "execution time = %s s" % (self.interval)
+
+
+class Timeout(object):
+    """
+    A Timeout handler, to handle functions that may hang
+    Use as:
+    try:
+        with Timeout(seconds):
+            # code
+    except TimeoutError:
+        # handle it
+    """
+    def __init__(self, seconds=1):
+        self.seconds = seconds
+    
+    def handle_timeout(self, *args):
+        raise excepts.TimeoutError()
+    
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    
+    def __exit__(self, *args):
+        signal.alarm(0)
