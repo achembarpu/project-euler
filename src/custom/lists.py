81f824dee74b795e80d075bf37a120cb7096f1e4
Arvind Chembarpu <achembarpu@gmail.com>
2014-04-19 18:03:21 +0530

    added comments to primes_list logic
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

--- src/custom/lists.py
+++ src/custom/lists.py
@@ -12,11 +12,11 @@ def primes_list(limit=params.prime_lim):
     Using a tweaked Sieve of Eratosthenes method
     """
     half = (limit // 2) + 1
-    sieve = [False, True] * half
+    sieve = [False, True] * half  # preset even nums False
     sieve[1], sieve[2] = False, True
-    bound = int(math.sqrt(limit)) + 1
+    bound = int(math.sqrt(limit)) + 1  # sqrt is loop bound
     for i in xrange(3, bound, 2):
-        if sieve[i]:
+        if sieve[i]:  # check if prime
             for j in xrange(i ** 2, limit, i):
-                sieve[j] = False
+                sieve[j] = False  # set prime's multiples False
     return [i for i in xrange(limit) if sieve[i]]
