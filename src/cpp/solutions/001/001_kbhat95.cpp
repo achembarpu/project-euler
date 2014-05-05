1a53c45a0d923b0e4976588d6afe652f63f563f8
Arvind Chembarpu <achembarpu@gmail.com>
2014-05-05 15:07:22 +0530

    updated timings
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

---
+++ src/cpp/solutions/001/001_kbhat95.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int i, sum = 0;
+    int lim = 1000;
+
+    for(i = 3;i < lim; i++)
+        if((i % 3 == 0) || (i % 5 == 0))
+            sum += i;
+
+    printf("%d\n", sum);
+    return 0;
+}
