27fea855611ed8c1a251f1de169ee64d048f03f0
Arvind Chembarpu <achembarpu@gmail.com>
2014-05-11 10:02:54 +0530

    optimized 001 cpp solution
    Signed-off-by: Arvind Chembarpu <achembarpu@gmail.com>

--- src/cpp/solutions/001/001_arvindch.cpp
+++ src/cpp/solutions/001/001_arvindch.cpp
@@ -6,13 +6,12 @@ int main()
 {
 	int giv_lim = 1000, req_sum = 0;
 
-	for(int n=0; n<giv_lim; n++)
-	{
-		if(n%3==0 || n%5==0)
-		{
-			req_sum += n;
-		}
-	}
+	for(int n=3; n<giv_lim; n+=3)
+		req_sum += n;
+	for(int n=5; n<giv_lim; n+=5)
+		req_sum += n;
+	for(int n=15; n<giv_lim; n+=15)
+		req_sum -= n;
 
 	cout << req_sum;
 	return 0;
